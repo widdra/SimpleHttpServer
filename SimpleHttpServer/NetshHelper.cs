f881845971689f8a2b9436212433f51562247267
philippwiddra <philipp.widdra@gmail.com>
2016-08-08 12:28:03 +0200

    Added communication to netsh command to allow nonadmin use

---
+++ SimpleHttpServer/NetshHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer
+{
+    public static class NetshHelper
+    {
+        public static void RegisterAddressForCurrentUser(string Address)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                FileName = "netsh",
+                Arguments = $"http add urlacl url={Address} user=\"{Environment.UserDomainName}\\{Environment.UserName}\" listen=yes",
+                Verb = "runas",
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = true
+            };
+
+            Process.Start(psi).WaitForExit();
+        }
+
+        private static readonly Regex userRegex = new Regex(@"^\W*Benutzer: ([a-zA-Z0-9.-_\\ ]+)\W*$", RegexOptions.Multiline);
+        public static string[] GetUserRegistrationForAddress(string Address)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                FileName = "netsh",
+                Arguments = $"http show urlacl {Address}",
+                Verb = "runas",
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+
+            Process p = Process.Start(psi);
+            p.WaitForExit();
+            string output = p.StandardOutput.ReadToEnd();
+
+            MatchCollection matches = userRegex.Matches(output);
+
+            string[] usernames = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+            {
+                usernames[i] = matches[i].Groups[1].Value;
+            }
+            return usernames;
+        }
+
+        public static bool AddressIsRegisteredForCurrentUser(string Address)
+        {
+            return GetUserRegistrationForAddress(Address).Contains($"{Environment.UserDomainName}\\{Environment.UserName}");
+        }
+    }
+}
--- SimpleHttpServer/Program.cs
+++ SimpleHttpServer/Program.cs
@@ -1,45 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SimpleHttpServer
 {
     class Program
     {
+        const int port = 8080;
+
         static void Main(string[] args)
         {
             Console.Title = "SimpleHttpServer";
+            string listenerPrefix = $"http://+:{port}/";
 
-            HttpListener httpListener = new HttpListener();
-            httpListener.Prefixes.Add("http://*:8080/");
-            httpListener.Start();
-
-            Console.WriteLine("Server listening on Port 8080");
-
-            while (true)
+            try
             {
-                HttpListenerContext context = httpListener.GetContext(); // waiting for connections
-                HttpListenerRequest req = context.Request;
-                HttpListenerResponse res = context.Response;
-
-                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {req.RemoteEndPoint} {req.HttpMethod} {req.Url}");
-
-                string payload = "Hello World!\n";
-
-                foreach (var queryItem in req.QueryString.AllKeys.Select(key => new { Key = key, Value = req.QueryString[key] }))
+                if (!NetshHelper.AddressIsRegisteredForCurrentUser(listenerPrefix))
                 {
-                    Console.WriteLine($"{queryItem.Key}: {queryItem.Value}");
+                    Console.WriteLine($"Press any key to allow the current user to access {listenerPrefix}");
+                    Console.ReadKey();
+                    NetshHelper.RegisterAddressForCurrentUser(listenerPrefix);
                 }
 
-                byte[] buffer = Encoding.UTF8.GetBytes(payload);
-                res.ContentLength64 = buffer.Length;
-                res.OutputStream.Write(buffer, 0, buffer.Length);
+                HttpListener httpListener = new HttpListener();
+                httpListener.Prefixes.Add(listenerPrefix);
+                httpListener.Start();
 
-                res.OutputStream.Close();
+                Console.WriteLine($"Server now listening on port {port}");
+
+                while (true)
+                {
+                    HttpListenerContext context = httpListener.GetContext(); // waiting for connections
+                    HttpListenerRequest req = context.Request;
+                    HttpListenerResponse res = context.Response;
+
+                    Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {req.RemoteEndPoint} {req.HttpMethod} {req.Url}");
+
+                    string payload = "Hello World!\n";
+
+                    foreach (var queryItem in req.QueryString.AllKeys.Select(key => new { Key = key, Value = req.QueryString[key] }))
+                    {
+                        Console.WriteLine($"{queryItem.Key}: {queryItem.Value}");
+                    }
+
+                    byte[] buffer = Encoding.UTF8.GetBytes(payload);
+                    res.ContentLength64 = buffer.Length;
+                    res.OutputStream.Write(buffer, 0, buffer.Length);
+
+                    res.OutputStream.Close();
+                }
+            }
+            catch (HttpListenerException ex) when (ex.ErrorCode == 5)
+            {
+                string[] allowedUsers = NetshHelper.GetUserRegistrationForAddress(listenerPrefix);
+
+                Console.WriteLine("Access denied.");
+                if (allowedUsers.Length == 0)
+                {
+                    Console.WriteLine($"There are no users registered for the address {listenerPrefix}");
+                }
+                else
+                {
+                    string allowedUsersString = allowedUsers.Select(x => $" - {x}").Aggregate((a, b) => $"{a}\n{b}");
+                    Console.WriteLine($"Allowed users for the address {listenerPrefix} are:\n{allowedUsersString}");
+                }
+                
+                Console.ReadKey();
             }
         }
     }
